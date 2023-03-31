1e528a6b5634adbeb49f16797bf0f1f81bbca7f6
dkim747 <narlaeogus@gmail.com>
2023-03-31 20:35:50 +0900

    230330자바 숙제

---
+++ JavaProgramming/src/chap999_homework_2023_03_29/_05_for.java
@@ -0,0 +1,45 @@
+package chap999_homework_2023_03_29;
+
+import java.util.Scanner;
+
+public class _05_for {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//1.
+		
+//		int sum = 0;
+//		for(int i = 1; i <= 10; i++) {
+//			sum = sum + i;			
+//		}
+//		System.out.println(sum);
+		
+		//2. 편법임
+		
+//		double result = 0;
+//		for(int i = 2; i <= 11; i++ ) {
+//			result = Math.pow(2, i);
+//			System.out.println(result);
+//		}
+		
+//		int result1 = 2;
+//		for(int i = 0; i < 10; i++) {
+//			 result1 = result1*2;
+//			 System.out.println(result1);
+//		}
+		
+		
+		//3.
+		
+		int input = sc.nextInt();
+		
+		
+		
+		
+		
+	}
+
+}
